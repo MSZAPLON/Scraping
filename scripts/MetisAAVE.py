8cf4243a94ea6753944127f9434f76e7d6157366
MSZAPLON <szmtacs.sze@gmail.com>
2023-10-26 01:25:37 +0200

    second try

--- scripts/MetisAAVE.py
+++ scripts/MetisAAVE.py
@@ -5,32 +5,8 @@ from datetime import datetime
 # specify the url
 url = "https://app.aave.com/reserve-overview/?underlyingAsset=0xdeaddeaddeaddeaddeaddeaddeaddeaddead0000&marketName=proto_metis_v3"
 
-import sys
-# chrome_options.add_argument("--headless")
-
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.utils import ChromeType
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-
-chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
-
-chrome_options = Options()
-options = [
-    "--headless",
-    "--disable-gpu",
-    "--window-size=1920,1200",
-    "--ignore-certificate-errors",
-    "--disable-extensions",
-    "--no-sandbox",
-    "--disable-dev-shm-usage"
-]
-for option in options:
-    chrome_options.add_argument(option)
-
-driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
-
+# create a new Chrome browser instance
+driver = webdriver.Chrome()
 
 # navigate to the url
 driver.get(url)
@@ -41,15 +17,34 @@ html = driver.page_source
 # parse the page source into BeautifulSoup
 soup = BeautifulSoup(html, 'html.parser')
 
-# specify the element to search for
-element = '<div class="MuiAlert-message css-1xsto0d">Asset cannot be used as collateral.</div>'
+# specify the elements to search for
+element1 = '<div class="MuiAlert-message css-1xsto0d">Asset cannot be used as collateral.</div>'
+element2 = '<p class="MuiTypography-root MuiTypography-main16 MuiTypography-noWrap css-1ogi1pb">'
+element3 = '<span class="MuiCircularProgress-root MuiCircularProgress-determinate MuiCircularProgress-colorSuccess css-1hz1jwd" role="progressbar" aria-valuenow="'
 
-# check if the element is in the page source
-if str(soup).find(element) != -1:
-    msg = "Asset cannot be used as collateral"
+# check if the elements are in the page source
+if str(soup).find(element1) != -1:
+    msg1 = "Asset cannot be used as collateral"
 else:
-    msg = "NOW NOW NOW"
-print(msg)
+    msg1 = "NOW NOW NOW"
+
+start2 = str(soup).find(element2)
+if start2 != -1:
+    end2 = str(soup).find('</p>', start2)
+    msg2 = str(soup)[start2+len(element2):end2]
+else:
+    msg2 = "Element not found"
+
+start3 = str(soup).find(element3)
+if start3 != -1:
+    end3 = str(soup).find('"', start3+len(element3))
+    msg3 = str(soup)[start3+len(element3):end3]
+else:
+    msg3 = "Element not found"
+
+print(msg1)
+print(msg2)
+print(msg3)
 
 # close the browser window
 driver.quit()
@@ -60,4 +55,4 @@ now = datetime.now()
 dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 
 with open("data/MetisAAVE.csv", "a") as text_file:
-    text_file.write(dt_string + "," + msg + '\n')
+    text_file.write(dt_string + "," + msg1 + "," + msg2 + "," + msg3 + '\n')
