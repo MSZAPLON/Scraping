4756cbe52769f113b1b5b5a115340f4f1a05bba6
MSZAPLON <szmtacs.sze@gmail.com>
2023-10-26 13:30:01 +0200

    exception handling

--- scripts/MetisAAVE.py
+++ scripts/MetisAAVE.py
@@ -35,53 +35,41 @@ html = driver.page_source
 driver.save_screenshot('screenshot.png')
 
 # get the elements
-supplied = driver.find_element(By.CSS_SELECTOR, ".MuiBox-root:nth-child(2) > .MuiBox-root > .MuiBox-root:nth-child(2) > .MuiBox-root > .MuiBox-root > .MuiTypography-root:nth-child(1)").text
-totalsupply = driver.find_element(By.CSS_SELECTOR, ".MuiBox-root:nth-child(2) > .MuiBox-root > .MuiBox-root:nth-child(2) .MuiTypography-root:nth-child(3)").text
-borrowed = driver.find_element(By.CSS_SELECTOR, ".MuiBox-root:nth-child(1) > .MuiBox-root:nth-child(1) > .MuiBox-root:nth-child(2) > .MuiBox-root > .MuiBox-root > .MuiTypography-root:nth-child(1)").text
-borrowcap = driver.find_element(By.CSS_SELECTOR, ".MuiBox-root:nth-child(1) > .MuiBox-root > .MuiBox-root > .MuiBox-root > .MuiBox-root > .MuiTypography-root:nth-child(3)").text
-apysupply = driver.find_element(By.CSS_SELECTOR, ".MuiBox-root:nth-child(2) > .MuiBox-root > .MuiBox-root:nth-child(3) > .MuiBox-root > .MuiTypography-root").text
-apyborrow = driver.find_element(By.CSS_SELECTOR, ".MuiBox-root:nth-child(1) > .MuiBox-root:nth-child(1) > .MuiBox-root:nth-child(3) > .MuiBox-root > .MuiTypography-root").text
-coll = driver.find_element(By.CSS_SELECTOR, ".MuiAlert-message").text
+try:
+    supplied = driver.find_element(By.CSS_SELECTOR, ".MuiBox-root:nth-child(2) > .MuiBox-root > .MuiBox-root:nth-child(2) > .MuiBox-root > .MuiBox-root > .MuiTypography-root:nth-child(1)").text
+except Exception:
+    supplied = None
 
-print(supplied)
-print(totalsupply)
-print(borrowed)
-print(borrowcap)    
-print(apysupply)
-print(apyborrow)
-print(coll)
+try:
+    totalsupply = driver.find_element(By.CSS_SELECTOR, ".MuiBox-root:nth-child(2) > .MuiBox-root > .MuiBox-root:nth-child(2) .MuiTypography-root:nth-child(3)").text
+except Exception:
+    totalsupply = None
 
-# parse the page source into BeautifulSoup
-soup = BeautifulSoup(html, 'html.parser')
+try:
+    borrowed = driver.find_element(By.CSS_SELECTOR, ".MuiBox-root:nth-child(1) > .MuiBox-root:nth-child(1) > .MuiBox-root:nth-child(2) > .MuiBox-root > .MuiBox-root > .MuiTypography-root:nth-child(1)").text
+except Exception:
+    borrowed = None
 
-# specify the elements to search for
-element1 = '<div class="MuiAlert-message css-1xsto0d">Asset cannot be used as collateral.</div>'
-element2 = '<p class="MuiTypography-root MuiTypography-main16 MuiTypography-noWrap css-1ogi1pb">'
-element3 = '<span class="MuiCircularProgress-root MuiCircularProgress-determinate MuiCircularProgress-colorSuccess css-1hz1jwd" role="progressbar" aria-valuenow="'
+try:
+    borrowcap = driver.find_element(By.CSS_SELECTOR, ".MuiBox-root:nth-child(1) > .MuiBox-root > .MuiBox-root > .MuiBox-root > .MuiBox-root > .MuiTypography-root:nth-child(3)").text
+except Exception:
+    borrowcap = None
 
-# check if the elements are in the page source
-if str(soup).find(element1) != -1:
-    msg1 = "Asset cannot be used as collateral"
-else:
-    msg1 = "NOW NOW NOW"
+try:
+    apysupply = driver.find_element(By.CSS_SELECTOR, ".MuiBox-root:nth-child(2) > .MuiBox-root > .MuiBox-root:nth-child(3) > .MuiBox-root > .MuiTypography-root").text
+except Exception:
+    apysupply = None
 
-start2 = str(soup).find(element2)
-if start2 != -1:
-    end2 = str(soup).find('</p>', start2)
-    msg2 = str(soup)[start2+len(element2):end2]
-else:
-    msg2 = "Element not found"
+try:
+    apyborrow = driver.find_element(By.CSS_SELECTOR, ".MuiBox-root:nth-child(1) > .MuiBox-root:nth-child(1) > .MuiBox-root:nth-child(3) > .MuiBox-root > .MuiTypography-root").text
+except Exception:
+    apyborrow = None
 
-start3 = str(soup).find(element3)
-if start3 != -1:
-    end3 = str(soup).find('"', start3+len(element3))
-    msg3 = str(soup)[start3+len(element3):end3]
-else:
-    msg3 = "Element not found"
+try:
+    coll = driver.find_element(By.CSS_SELECTOR, ".MuiAlert-message").text
+except Exception:
+    coll = None
 
-print(msg1)
-print(msg2)
-print(msg3)
 
 # close the browser window
 driver.quit()
@@ -92,4 +80,4 @@ now = datetime.now()
 dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 
 with open("data/MetisAAVE.csv", "a") as text_file:
-    text_file.write(dt_string + "," + msg1 + "," + msg2 + "," + msg3 + '\n')
+    text_file.write(dt_string + "," + str(coll) + "," + str(supplied) +  "," + str(totalsupply) + "," + str(apysupply) + "," + str(borrowed) + "," + str(borrowcap) + "," + str(apyborrow) + '\n')
