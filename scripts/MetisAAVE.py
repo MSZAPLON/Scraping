a7b631c0e8dcbd2ce7700f4c02d12f0e0107d226
MSZAPLON <szmtacs.sze@gmail.com>
2023-10-26 13:18:14 +0200

    Update MetisAAVE.py

--- scripts/MetisAAVE.py
+++ scripts/MetisAAVE.py
@@ -7,6 +7,13 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
+from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.action_chains import ActionChains
+# from selenium.webdriver.support import expected_conditions
+# from selenium.webdriver.support.wait import WebDriverWait
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
 # specify the url
 url = "https://app.aave.com/reserve-overview/?underlyingAsset=0xdeaddeaddeaddeaddeaddeaddeaddeaddead0000&marketName=proto_metis_v3"
 
