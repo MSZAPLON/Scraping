8c380643b1a56a0b013953c4264c29ecb3c34dda
MSZAPLON <39549352+MSZAPLON@users.noreply.github.com>
2024-01-06 23:16:33 +0100

    Update MetisAAVE.py

--- scripts/MetisAAVE.py
+++ scripts/MetisAAVE.py
@@ -108,12 +108,12 @@ except Exception:
     borrowcap = None
 
 try:
-    apysupply = driver.find_element(By.CSS_SELECTOR, ".MuiBox-root:nth-child(2) > .MuiBox-root > .MuiBox-root:nth-child(3) > .MuiBox-root > .MuiTypography-root").text.replace('\n', '').replace(',', '').replace('%', '')
+    apysupply = driver.find_element(By.CSS_SELECTOR, ".MuiBox-root:nth-child(2) > .MuiBox-root > .MuiBox-root:nth-child(3) > .MuiBox-root > .MuiTypography-root").text.replace('\n', '').replace(',', '').replace('%', '').replace('<', '').replace('>', '')
 except Exception:
     apysupply = None
 
 try:
-    apyborrow = driver.find_element(By.CSS_SELECTOR, ".MuiBox-root:nth-child(1) > .MuiBox-root:nth-child(1) > .MuiBox-root:nth-child(3) > .MuiBox-root > .MuiTypography-root").text.replace('\n', '').replace(',', '').replace('%', '')
+    apyborrow = driver.find_element(By.CSS_SELECTOR, ".MuiBox-root:nth-child(1) > .MuiBox-root:nth-child(1) > .MuiBox-root:nth-child(3) > .MuiBox-root > .MuiTypography-root").text.replace('\n', '').replace(',', '').replace('%', '').replace('<', '').replace('>', '')
 except Exception:
     apyborrow = None
 
